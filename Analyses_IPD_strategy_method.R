3402ed867fa84210e46b637f551313e4b965238d
Louis Nurdin <nurdin@gael-labo.u-ga.fr>
2026-06-10 09:13:27 +0200

    modif chemin d'accès

--- Analyses_IPD_strategy_method.R
+++ Analyses_IPD_strategy_method.R
@@ -19,11 +19,10 @@ library(tidyr)
 library(pwr)
 library(tidyverse)
 
-setwd("/Users/simonvaraine/Documents/Doctorat/Ecriture d'articles/IPD strategy method/Analyses")
 
 # Lire les données simulées
 #df <- read_excel("Simulated data/data_IPD_simulated.xlsx")
-df <- read.csv("/Users/simonvaraine/Documents/Doctorat/Ecriture\ d\'articles/IPD\ strategy\ method/Analyses/Raw\ data/data_all.csv")
+df <- read.csv("ipd/IPD/data/data_all.csv")
 
 df <- unique(df)
 df <- df[!is.na(df$pd_in3_out3),]
